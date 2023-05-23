e18b63e86cc66ce39965af03318156594ce3505b
D <dhy@jshxlsp.com>
2023-05-23 11:06:18 +0800

    fix: 层级嵌套bug

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,189 +1,37 @@
-import React, { useRef, useState } from 'react';
-import {
-  FlatList,
-  Pressable,
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-import DynamicForm from './DynamicForm';
+import React from 'react';
+import { View, TextInput, Button, Text } from 'react-native';
+import { useForm, Controller } from 'react-hook-form';
 
-type ItemData = {
-  id: string;
-  title: string;
-};
+export default function App() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-let uId = 1;
-
-const DATA: ItemData[] = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'First Item',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
-  },
-];
-
-type ItemProps = {
-  item: ItemData;
-  onPress: () => void;
-  backgroundColor: string;
-  textColor: string;
-};
-
-const Item = ({
-  item,
-  onPress,
-  backgroundColor,
-  textColor,
-  index,
-}: ItemProps) => (
-  <View style={[styles.item, { backgroundColor }]}>
-    <DynamicForm />
-    <Text style={[styles.title, { color: textColor }]}>{item.title}</Text>
-    <Text style={[styles.title, { color: textColor }]}>{item.title}</Text>
-
-    <Text style={[styles.title, { color: textColor }]}>{item.title}</Text>
-
-    {index % 3 === 0 ? (
-      <>
-        <Text style={[styles.title, { color: textColor }]}>{item.title}</Text>
-
-        <Text style={[styles.title, { color: textColor }]}>{item.title}</Text>
-
-        <Text style={[styles.title, { color: textColor }]}>{item.title}</Text>
-
-        <Text style={[styles.title, { color: textColor }]}>{item.title}</Text>
-        <Text style={[styles.title, { color: textColor }]}>{item.title}</Text>
-        <Text style={[styles.title, { color: textColor }]}>{item.title}</Text>
-
-        <Text style={[styles.title, { color: textColor }]}>{item.title}</Text>
-
-        <Text style={[styles.title, { color: textColor }]}>{item.title}</Text>
-
-        <Text style={[styles.title, { color: textColor }]}>{item.title}</Text>
-        <Text style={[styles.title, { color: textColor }]}>{item.title}</Text>
-
-        <Text style={[styles.title, { color: textColor }]}>{item.title}</Text>
-        <Text style={[styles.title, { color: textColor }]}>{item.title}</Text>
-
-        <Text style={[styles.title, { color: textColor }]}>{item.title}</Text>
-
-        <Text style={[styles.title, { color: textColor }]}>{item.title}</Text>
-      </>
-    ) : null}
-  </View>
-);
-
-const App = () => {
-  const [selectedId, setSelectedId] = useState<string>();
-  const [visibleIndex, setVisibleIndex] = useState(0);
-
-  const handleScroll = (e) => {
-    const offsetY = e.nativeEvent.contentOffset.y;
-
-    const contentHeight = e.nativeEvent.contentSize.height;
-
-    const scrollViewHeight = e.nativeEvent.layoutMeasurement.height;
-
-    const currentPercentage = (offsetY + scrollViewHeight) / contentHeight;
-    const index = Math.round(currentPercentage * DATA.length);
-
-    setVisibleIndex(index);
+  const onSubmit = (data) => {
+    console.log(data);
   };
-  const renderItem = ({ item, index }: { item: ItemData }) => {
-    const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#f9c2ff';
-    const color = item.id === selectedId ? 'white' : 'black';
 
-    return (
-      <Item
-        item={item}
-        index={index}
-        onPress={() => setSelectedId(item.id)}
-        backgroundColor={backgroundColor}
-        textColor={color}
-      />
-    );
-  };
-  const ref = useRef(null);
   return (
-    <SafeAreaView style={styles.container}>
-      <Pressable
-        onPress={() => {
-          ref.current.scrollToIndex({
-            index: DATA.length - 2,
-            animated: true,
-          });
-        }}
-      >
-        <Text>Scroll TO last</Text>
-      </Pressable>
-      <Text style={{ textAlign: 'center', padding: 20 }}>
-        Currently visible item: {visibleIndex}
-      </Text>
-      <FlatList
-        scrollEventThrottle={300}
-        keyExtractor={() => `${uId++}`}
-        onScroll={handleScroll}
-        ref={ref}
-        data={DATA}
-        renderItem={renderItem}
-        extraData={selectedId}
+    <View style={{ flex: 1, justifyContent: 'center', padding: 16 }}>
+      <Controller
+        control={control}
+        name="a.b.c"
+        rules={{ required: 'This field is required' }}
+        render={({ field }) => (
+          <TextInput
+            onChangeText={field.onChange}
+            onBlur={field.onBlur}
+            value={field.value}
+            placeholder="Enter a.b.c"
+          />
+        )}
       />
-    </SafeAreaView>
+      {errors?.a?.b?.c && (
+        <Text style={{ color: 'red' }}>{errors.a.b.c.message}</Text>
+      )}
+      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+    </View>
   );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
-  item: {
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
-  title: {
-    fontSize: 32,
-  },
-});
-
-export default App;
+}
